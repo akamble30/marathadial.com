5bd00f4f1a46f241f7db21a316faa7be06c6bec1
LENOVO <LENOVO@DESKTOP-9HTRVJE>
2018-02-27 12:36:02 +0530

    firox setting

--- SeleniumDemo/src/selenium/practice/Button.java
+++ SeleniumDemo/src/selenium/practice/Button.java
@@ -1,18 +1,30 @@
 package selenium.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Button {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\LENOVO\\Desktop\\Java64\\Chromedriver.exe");
-		 ChromeDriver driver = new ChromeDriver();
+	//	System.setProperty("webdriver.chrome.driver","C:\\Users\\LENOVO\\Desktop\\Java64\\Chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\LENOVO\\Desktop\\Java64\\geckodriver.exe");
+		FirefoxOptions option = new FirefoxOptions();
+		Options.setBinary("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		WebDriver driver = new FirefoxDriver(option);
+		
+		// ChromeDriver driver = new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		
-		driver.get("https://www.google.co.in");
-		boolean button=driver.findElementByName("btnK").isDisplayed();
-		System.out.println(button);
+		driver.get("http://www.marathadial.com/");
+	
+	
+		
+		
 
 	}
 
